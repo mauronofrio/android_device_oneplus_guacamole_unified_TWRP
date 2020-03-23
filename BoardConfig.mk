2f10cab1ce4402205cd74994744d1601ba14908d
mauronofrio <poronofrio@gmail.com>
2020-03-23 20:33:56 +0100

    Specify kernel image name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,6 +50,7 @@ BOARD_KERNEL_CMDLINE += skip_override androidboot.fastboot=1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_PREBUILT_KERNEL := device/oneplus/guacamole/prebuilt/Image.gz-dtb
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
